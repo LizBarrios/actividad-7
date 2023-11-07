76e9b7dec0f2c7b7c93ca22aef3400621db3c3ea
LizBarrios <lizeth.retana@uabc.edu.mx>
2023-11-06 22:44:02 -0800

    modified:   parte_1.exe 	modified:   parte_2.c 	deleted:    parte_2.exe

--- parte_2.c
+++ parte_2.c
@@ -85,7 +85,7 @@ int main()
             printf("Cadena validada: %s\n", cadena);
             break;
         case 8:
-            validarletras("Ingrese una cadena: ", cadena);
+            validarCadenaConRestricciones("Ingrese una cadena: ", cadena);
             if (esPalindromo(cadena))
             {
                 printf("La cadena es un palindromo.\n");
